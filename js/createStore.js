dfe2216537396013137820f7a58562d65c4554d8
Leah Silber <LeahSilber@gmail.com>
2020-05-29 11:51:28 -0700

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,43 @@
-let state;
+function createStore() {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState,
+  };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case "INCREASE_COUNT":
       return { count: state.count + 1 };
 
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+}
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
+}
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+dispatch({ type: "@@INIT" });
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+let button = document.getElementById("button");
+
+let store = createStore();
+store.dispatch({ type: "@@INIT" });
+
+button.addEventListener("click", () => {
+  store.dispatch({ type: "INCREASE_COUNT" });
+});
